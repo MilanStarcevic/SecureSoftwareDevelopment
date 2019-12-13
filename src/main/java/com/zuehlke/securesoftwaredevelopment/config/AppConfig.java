e4f0ef564d637cc3cd46a777ca047ed6686ca023
Djordje Madic <djordje.madic@swisscom.com>
2019-12-13 09:39:38 +0100

    Add simple layout

---
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/config/AppConfig.java
@@ -0,0 +1,14 @@
+package com.zuehlke.securesoftwaredevelopment.config;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+
+public class AppConfig {
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.addDialect(new LayoutDialect());
+        return templateEngine;
+    }
+}
