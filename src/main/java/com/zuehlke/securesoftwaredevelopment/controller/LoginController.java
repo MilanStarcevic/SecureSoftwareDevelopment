d73a65ff6f0794dbcb01ed9c3b38fc02ea6eac6a
TAASTMIO <milan.starcevic@swisscom.com>
2020-04-16 09:14:36 +0200

    2nd factor authentication experiment

--- src/main/java/com/zuehlke/securesoftwaredevelopment/controller/LoginController.java
+++ src/main/java/com/zuehlke/securesoftwaredevelopment/controller/LoginController.java
@@ -1,7 +1,12 @@
 package com.zuehlke.securesoftwaredevelopment.controller;
 
+import com.warrenstrange.googleauth.GoogleAuthenticator;
+import com.warrenstrange.googleauth.GoogleAuthenticatorKey;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
@@ -9,5 +14,22 @@ public class LoginController {
     @GetMapping("/login")
     public String showLoginForm() {
         return "login";
+    }
+    // send totp to DatabaseAuthenticationProvider
+    // Check using https://github.com/wstrange/GoogleAuth
+
+    @GetMapping("/register-totp")
+    public String showRegisterTotp(Model model) {
+        GoogleAuthenticator gAuth = new GoogleAuthenticator();
+        final GoogleAuthenticatorKey key = gAuth.createCredentials();
+
+        model.addAttribute("totpKey", key.getKey());
+        return "totp";
+    }
+
+    @PostMapping("/register-totp")
+    public String registerTotp(@RequestParam() String totpKey) {
+        // save totp to user table
+        return "login"; // change url
     }
 }
